af0d18989f77c7b791a959eba279524a14dc0549
avivek8694 <61772917+avivek8694@users.noreply.github.com>
2023-01-28 14:10:22 +0530

    Add files via upload

---
+++ Section1_PythonBasics.py
@@ -0,0 +1,8 @@
+# This program says hi and asks for my name and age
+
+print('Hi, What is your name ?')
+myName = input()
+print('Your name is ' + str(len(myName)) + ' chars long.')
+print('What is your age ?')
+myAge = input()
+print('Oh, you are ' + myAge + ' years old.')
